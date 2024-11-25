99a584c6bfae3a8a7b34c14dd73fd6d5449e54da
stefanocasafranca <65425617+stefanocasafranca@users.noreply.github.com>
2024-11-25 13:36:04 -0800

    Initial Commit

---
+++ Monopoly3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monopoly3
+//
+//  Created by Stefano Casafranca Laos on 11/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Monopoly3Tests/Monopoly3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Monopoly3Tests.swift
+//  Monopoly3Tests
+//
+//  Created by Stefano Casafranca Laos on 11/25/24.
+//
+
+import Testing
+@testable import Monopoly3
+
+struct Monopoly3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
